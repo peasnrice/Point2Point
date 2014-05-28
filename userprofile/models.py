f32c1a58cc66f617b211ea259fac7bd448063484
Andy <andrew.j.c.e@gmail.com>
2014-05-28 17:26:00 -0400

    UserProfiles are now related to Competitions
    Be careful when using south, every app needs to be migrated
    individually it seems.
    By connecting userProfiles to competitions we can now record which
    competitions a user has completed.

--- userprofile/models.py
+++ userprofile/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-	user = models.OneToOneField(User)
-	likes_cheese = models.BooleanField(default=True)
-	favourite_hamster_name = models.CharField(max_length=50)
+    user = models.OneToOneField(User)
+    competitions = models.ManyToManyField("quests.Competition", blank=True, null=True)
+    likes_cheese = models.BooleanField(default=True)
+    favourite_hamster_name = models.CharField(max_length=50)
+    def __unicode__(self):
+        return self.user.username
 
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
